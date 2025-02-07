35dd832a14b0f9ef58c3ec2534a87b4ddf235990
OCCHIO NERO <112170802+Baldesito@users.noreply.github.com>
2025-02-07 15:06:39 +0100

    Primo commit

---
+++ src/main/java/com/example/spring_jdbc_project/SpringJdbcProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_jdbc_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringJdbcProjectApplication.class, args);
+		System.out.println("run....");
+	}
+
+}
